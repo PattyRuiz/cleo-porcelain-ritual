a91b55e05a6374640edb6725b947d5ed97e6bf8d
PattyRuiz <patty.ruiz@gmail.com>
2025-09-17 20:35:37 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/cleo-porcelain-ritual/", // 👉 Agrega esta línea
   server: {
     host: "::",
     port: 8080,
